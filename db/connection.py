21c01e1f520c4fffd5d0d2adf824d574e4c2f81c
Varun <mangevarun2006@gmail.com>
2025-11-27 13:17:00 -0600

    modified the templates to use the base html file and fixed the database connection

--- db/connection.py
+++ db/connection.py
@@ -6,18 +6,7 @@ from psycopg2.pool import SimpleConnectionPool
 
 load_dotenv()
 
-# database_url = urlparse(os.getenv("DATABASE_URL"))
 database_url = os.getenv("DATABASE_URL")
-
-# pool = SimpleConnectionPool(
-#     1,
-#     10,
-#     dbname=database_url.path[1:],
-#     user=database_url.username,
-#     password=database_url.password,
-#     host=database_url.hostname,
-#     port=database_url.port,
-# )
 
 pool = SimpleConnectionPool(1, 10, database_url, cursor_factory=RealDictCursor)
 
